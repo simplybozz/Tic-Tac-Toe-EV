6efd126812bd17e5c48480b900c18e0081b53228
simplybozz <angelito.com06@gmail.com>
2024-10-14 00:21:29 -0400

    Actualizacion de comentarios en algunos html y js

--- funciones/loading.js
+++ funciones/loading.js
@@ -1,15 +1,20 @@
+// Ejecutar la función cuando la ventana se haya cargado completamente
 window.onload = function() {
 
-    const progressFill = document.querySelector('.progreso');
+    // Obtener el elemento de la barra de progreso
+    const progressFill = document.querySelector('.progreso'); 
 
-    let progress = 0;
+    let progress = 0;  // Iniciar el progreso en 0
+
+    // Crear un intervalo que incrementa el progreso cada 20ms
     const interval = setInterval(() => {
-        progress += 1;
+        progress += 1;  // Incrementar el progreso en 1 cada vez
         progressFill.style.width = `${progress}%`;
 
+        // Si el progreso alcanza o supera el 100%, detener el intervalo y redirigir
         if (progress >= 100) {
             clearInterval(interval);
             window.location.href = "menuPrincipal.html";
         }
-    }, 20);
+    }, 20);  // Intervalo de 20 milisegundos
 };
